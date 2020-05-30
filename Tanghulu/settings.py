3a1dfa1dd647b8c1d6876b8376fde7d1dcd6be68
Charles <dirtybluer@icloud.com>
2020-05-30 08:44:05 +0800

    modify settings.py to connect to MySQL

--- Tanghulu/settings.py
+++ Tanghulu/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'Tanghulu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Tanghulu',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'tanghulu',
+        'PASSWORD': 'Tanghulu.1961',
+                        }
+            }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -115,3 +119,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
